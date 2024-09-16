206c3d8ad9fb038918245bb6601c2058fdbaffb2
rayir03 <rayir03@gmail.com>
2024-09-15 21:24:18 -0500

    Finish

--- interactive-rating-component-main/src/App.jsx
+++ interactive-rating-component-main/src/App.jsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Star from './assets/images/icon-star.svg'
+import Thankyou from "./assets/images/illustration-thank-you.svg"
 import './App.css'
+import { useState } from 'react'
+import {motion} from "framer-motion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ selectedRating , setSelectedRating ] = useState();
+  const [ isSelected, setSelected ] = useState(false);
+
+  const handleSelectedRating = (rating) => {
+    setSelectedRating(rating)
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setSelected(true);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isSelected ? (
+        <motion.div 
+          className="thank-you"
+          initial={{scale: 0.5}}
+          animate={{scale: 1}}
+          exit={{scale: 0}}
+          transition={{duration: 1}}
+        >
+          <img src={Thankyou} alt="" />
+          <span className="rating">You selected {selectedRating} out of 5 </span>
+          <h2>Thank you!</h2>
+          <p>
+            We appreciate you taking the time to give a rating. If you ever need more support, 
+            don’t hesitate to get in touch!
+          </p>
+        </motion.div>
+
+      ):(
+        <motion.form 
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          transition={{duration: 2}}
+          onSubmit={submitForm} 
+          className="rating-container"
+        >
+          <div className="star">
+            <img  src={Star} alt="Star" />
+          </div>
+          <h1>How did we do?</h1>
+
+          <p> 
+              Please let us know how we did with your support request. All feedback is appreciated 
+            to help us improve our offering!
+          </p>
+
+          <div className="rating-numbers">
+            {[1,2,3,4,5].map((rating) => (
+              <button className='rating-btn' key={rating} onClick={() => handleSelectedRating(rating)} type="button">
+                {rating}
+              </button>
+            ))}
+          </div>
+          <button className="btn">Submit</button>
+        </motion.form>
+      )}
+      
+      
     </>
   )
 }
